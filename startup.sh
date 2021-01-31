18269e1a8b7c0a9e35c343fdece21a39b6c23b51
Lucaribou <therealapex@gmail.com>
2021-01-31 16:29:45 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -12,4 +12,4 @@ sudo chmod 600 /etc/smbcredentials/storageaccountuser.cred
 sudo bash -c 'echo "//storageaccount.file.core.windows.net/fileshare /mnt/biggs-fs cifs nofail,vers=3.0,credentials=/etc/smbcredentials/storageaccountuser.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
 sudo mount -t cifs //storageaccount.file.core.windows.net/fileshare /mnt/biggs-fs -o vers=3.0,credentials=/etc/smbcredentials/storageaccountuser.cred,dir_mode=0777,file_mode=0777,serverino
 
-/home/lucaribou/broadcast.sh
+/home/user/broadcast.sh
